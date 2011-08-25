5236002b4728846e0662cddcc70519058c8a0caf
Ryan R. Smith <this.ryansmith@gmail.com>
2011-08-25 11:19:51 -0700

    starting to refactor that huge if-then into the model

--- lib/shushu/billable_event.rb
+++ lib/shushu/billable_event.rb
@@ -2,26 +2,38 @@ class Shushu::BillableEvent < Sequel::Model
 
   ATTRS = [:provider_id, :resource_id, :event_id, :reality_from, :reality_to, :qty, :rate_code]
 
-  def similar?(hash)
-    tmp_self = values.dup
-
-    tmp_self.delete(:id)
-    tmp_self.delete(:system_from)
-    tmp_self.delete(:system_to)
-
-    tmp_self.all? {|k,v| hash[k].to_s == v.to_s}
+  def self.find_or_instantiate_by_provider_and_event(provider_id, event_id)
+    params = {:provider_id => provider_id, :event_id => event_id}
+    filter(params).first or new(params)
   end
 
   def validate
     super
+
+    if !new?
+      detect_incorrect_data_change
+    end
   end
 
-  def valid?
-    true
+  def detect_incorrect_data_change
+    if !only_modifying_reality_to?
+      changed_from_non_nil_columns.each {|col| errors.add(col, "no longer able to modify") }
+    end
   end
 
-  def to_hash
-   {}
+  def only_modifying_reality_to?
+    if modified?
+      changed_columns == [:reality_to]
+    else
+      true
+    end
+  end
+
+  # Say that reality_to is nil in the database and qty is not nil. Also say that
+  # we have set both qty and reality_to to some non-nil value.
+  # This method will tell us that qty is the only changed column.
+  def changed_from_non_nil_columns
+    changed_columns.reject {|col| self.class[self.id][col].nil? }
   end
 
 end
--- test/billable_event_test.rb
+++ test/billable_event_test.rb
@@ -2,7 +2,22 @@ require File.expand_path('../test_helper', __FILE__)
 
 class BillableEventTest < Shushu::Test
 
-  def test_validates_change_of_created_at
+  def test_not_valid_when_changing_reality_from
+    event = Shushu::BillableEvent.create(:reality_from => Time.mktime(2011,1), :qty => 1, :rate_code => "SG001")
+    event.set(:qty => 2, :reality_to => Time.now)
+    assert(!event.valid?)
+    assert_equal({:qty => ["no longer able to modify"]}, event.errors)
+  end
+
+  def test_detect_incorrect_data_change_when_modify_qty
+    event = Shushu::BillableEvent.create(:reality_from => Time.mktime(2011,1), :qty => 1, :rate_code => "SG001")
+
+    event.detect_incorrect_data_change
+    assert event.errors.empty?
+
+    event.set(:qty => 2)
+    event.detect_incorrect_data_change
+    assert(!event.errors.empty?)
   end
 
 end
