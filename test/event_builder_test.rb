5c369474538c38442754db2312565f7ad8e3431d
Ryan R. Smith <this.ryansmith@gmail.com>
2011-09-09 11:32:31 -0700

    Improved testing
    Attempting to cover all of the cases for the public API of this
    module

--- test/event_builder_test.rb
+++ test/event_builder_test.rb
@@ -36,9 +36,37 @@ class EventBuilderTest < Shushu::Test
     BillableEvent.create(args)
     _, event = EventBuilder.handle_incomming(:provider_id => @provider.id, :event_id => '123', :reality_to => Time.mktime(2011,1,15))
     assert event.errors.length.zero?
-    assert_equal 2, BillableEvent.count
     assert_equal Time.mktime(2011,1,15), event.reality_to
     assert_nil event.system_to
+  end
+
+  def test_handle_incoming_creates_another_record_on_close
+    args = {
+      :provider_id    => @provider.id,
+      :event_id       => '123',
+      :resource_id    => "app123",
+      :qty            => 1,
+      :rate_code      => "RT01",
+      :reality_from   => Time.mktime(2011,1)
+    }
+    BillableEvent.create(args)
+    assert_equal 1, BillableEvent.count
+    _, event = EventBuilder.handle_incomming(:provider_id => @provider.id, :event_id => '123', :reality_to => Time.mktime(2011,1,15))
+    assert_equal 2, BillableEvent.count
+  end
+
+  def test_handle_incoming_sets_system_to_on_existing_record
+    args = {
+      :provider_id    => @provider.id,
+      :event_id       => '123',
+      :resource_id    => "app123",
+      :qty            => 1,
+      :rate_code      => "RT01",
+      :reality_from   => Time.mktime(2011,1)
+    }
+    existing_event = BillableEvent.create(args)
+    _, event = EventBuilder.handle_incomming(:provider_id => @provider.id, :event_id => '123', :reality_to => Time.mktime(2011,1,15))
+    assert ! existing_event.reload.system_to.nil?
   end
 
 end
