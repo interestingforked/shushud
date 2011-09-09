ad61a0854ce27d7914291554285ddb173c1987bb
Ryan R. Smith <this.ryansmith@gmail.com>
2011-09-09 11:21:58 -0700

    Test authentication
    the authenticate method should set some session and return true

---
+++ test/authentication_test.rb
@@ -0,0 +1,50 @@
+require File.expand_path('../test_helper', __FILE__)
+
+class AuthenticationTest < Shushu::Test
+
+  class TestAuth
+    attr_reader :params, :env
+    def initialize
+      @params = {}
+      @env = {}
+    end
+
+    def request
+      def env
+        @env
+      end
+      self
+    end
+  end
+
+  def setup
+    super
+    @auth = TestAuth.new
+    @auth.send(:extend, Shushu::Web::Authentication)
+
+    @provider = Shushu::Provider.create(:token => "abc123")
+  end
+
+  def test_authenticate_sets_params
+    @auth.authenticate(@provider.id, @provider.token)
+    assert_equal @provider.id, @auth.params[:provider_id]
+  end
+
+  def test_authenticate_sets_request_env
+    @auth.authenticate(@provider.id, @provider.token)
+    assert_equal @provider.id, @auth.request.env["PROVIDER_ID"]
+  end
+
+  def test_authenticate_returns_false_when_not_valid_provider_token
+    assert ! @auth.authenticate(@provider.id, (@provider.token + "bad"))
+  end
+
+  def test_authenticate_returns_false_when_not_valid_provider_id
+    assert ! @auth.authenticate(99999, @provider.token)
+  end
+
+  def test_authenticate_returns_true_when_valid_provider_id_and_token
+    assert @auth.authenticate(@provider.id, @provider.token)
+  end
+
+end
