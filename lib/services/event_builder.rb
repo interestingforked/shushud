7e67c93ced5165c20313cdf3db2bbdc8beb8c656
Ryan R. Smith <this.ryansmith@gmail.com>
2011-10-26 11:08:43 -0700

    decompose method

--- lib/services/event_builder.rb
+++ lib/services/event_builder.rb
@@ -3,10 +3,10 @@ class EventBuilder
   # => open / close events, notify open / close, return http status code
 
   # Abstract:
-  # The EventBuilder provides the handle_incomming(args) interface to
-  # some sort of public api, be it http or otherwise.
+  # The EventBuilder provides the handle_incomming(args) & find(conds)
+  # interface to some sort of public api, be it http or otherwise.
   #
-  # HTTP -> EventBuilder -> EventHandler (billable_events_table | message_bus | etc...)
+  # HTTP -> EventBuilder -> EventHandler (billable_events_table | message_bus | whatever...)
   #
 
   def initialize(handler)
@@ -20,24 +20,33 @@ class EventBuilder
   def handle_incomming(args)
     log("handle incomming args=#{args}")
     if existing = @handler.find_open(args[:provider_id], args[:event_id])
-      eid = existing.id
-      log("found existing billable_event=#{eid}")
-      if EventValidator.invalid?(existing, args)
-        log("attempting to change field")
-        [409, existing]
-      elsif close_dt = args[:reality_to] #wants to close event
-        log("closing event=#{eid}")
-        event = @handler.close(eid, close_dt)
-        [200, event]
-      else
-        log("existing is identical to new")
-        [200, existing]
-      end
+      log("found existing billable_event=#{existing.id}")
+      try_close(existing, args)
     else
       log("open billable_event args=#{args}")
-      event = @handler.open(args)
-      [201, event]
+      open(args)
     end
+  end
+
+  private
+
+  def try_close(existing, args)
+    eid = existing.id
+    if EventValidator.invalid?(existing, args)
+      log("attempting to change field")
+      [409, existing]
+    elsif close_dt = args[:reality_to] #wants to close event
+      log("closing event=#{eid}")
+      event = @handler.close(eid, close_dt)
+      [200, event]
+    else
+      log("existing is identical to new")
+      [200, existing]
+    end
+  end
+
+  def open(args)
+    [201, @handler.open(args)]
   end
 
 end
