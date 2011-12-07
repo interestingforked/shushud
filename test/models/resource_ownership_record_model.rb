027b549cee190052215d6820167c57fc85d95ada
Ryan R. Smith (ace hacker) <this.ryansmith@gmail.com>
2011-12-07 14:46:17 -0800

    test that our join works

--- test/models/resource_ownership_record_model.rb
+++ test/models/resource_ownership_record_model.rb
@@ -9,14 +9,14 @@ class ResourceOwnershipRecordTest < ShushuTest
     assert(!records.empty?, "Expected to find some records")
   end
 
-=begin
   def test_find_returns_from_and_to
     account = build_account
-    build_resource_ownership_record(:hid => "123", :time => jan, :state => ResourceOwnershipRecord::Active)
+    build_resource_ownership_record(:account_id => account.id, :hid => "123", :time => jan, :state => ResourceOwnershipRecord::Active)
+    build_resource_ownership_record(:account_id => account.id, :hid => "123", :time => feb, :state => ResourceOwnershipRecord::Inactive)
     records = ResourceOwnershipRecord.find(account.id, jan, feb)
-    assert(!records.empty?, "Expected to find some records")
+    assert_equal(1, records.count)
+    assert_equal(feb, records.first[:to])
   end
-=end
 
   def jan
     Time.mktime(2011,1)
