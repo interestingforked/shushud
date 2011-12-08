46fefe700fa7933c00097894b4ffe82d074d2f52
Ryan R. Smith (ace hacker) <this.ryansmith@gmail.com>
2011-12-07 17:41:04 -0800

    remove dup file

--- test/api/resource_ownership_api_test.rb
+++ test/api/resource_ownership_api_test.rb
@@ -2,8 +2,15 @@ require File.expand_path('../../test_helper', __FILE__)
 
 class ResourceOwnershipApiTest < ShushuTest
 
-  def test_create_record
+  def test_activate_record
     post "/resource_ownership", {:account_id => account.id, :hid => "123"}
+    assert_equal 201, last_response.status
+  end
+
+  def test_transfer_record
+    second_account = build_account
+    post "/resource_ownership", {:account_id => account.id, :hid => "123"}
+    put "/resource_ownership", {:prev_account_id => account.id, :account_id => second_account.id, :hid => "123"}
     assert_equal 201, last_response.status
   end
 
