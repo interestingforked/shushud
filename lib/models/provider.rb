386f8413c0fedbecf5047bd8914220d576b2855c
Ryan R. Smith (ace hacker) <this.ryansmith@gmail.com>
2011-12-08 16:25:48 -0800

    whitespace

--- lib/models/provider.rb
+++ lib/models/provider.rb
@@ -1,11 +1,11 @@
 class Provider < Sequel::Model
-  
+
   def root?
     self[:root]
   end
-  
+
   def write_to_billable_events?
     self[:billable_events] == true
   end
-  
+
 end
--- lib/models/rate_code.rb
+++ lib/models/rate_code.rb
@@ -1,9 +1,9 @@
 class RateCode < Sequel::Model
 
   PUBLIC_ATTRS = [:slug, :rate, :description]
-  
+
   def api_values
     values.select {|v| PUBLIC_ATTRS.include?(v)}
   end
-  
+
 end
