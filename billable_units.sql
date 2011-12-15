92f797f1044e6277f9d62e26743e8bd430c8ff28
Ryan R. Smith (ace hacker) <this.ryansmith@gmail.com>
2011-12-15 00:57:34 -0800

    add event_id to resource_ownerhips and document sql functions

--- billable_units.sql
+++ billable_units.sql
@@ -1,16 +1,28 @@
+-- dev strategy: /i this file and then rollback;
 BEGIN;
 
-INSERT INTO accounts (id) VALUES (123);
-INSERT INTO providers (id) VALUES (1);
-INSERT INTO rate_codes (id) VALUES (1);
+SET timezone TO 'UTC';
 
+/*
+  Helpers
+*/
 
-INSERT INTO resource_ownership_records (account_id, hid, time, state) VALUES (123, 'app123', '2011-12-01 00:00:00 UTC', 'active');
-INSERT INTO resource_ownership_records (account_id, hid, time, state) VALUES (123, 'app124', '2011-12-01 00:00:00 UTC', 'active');
-INSERT INTO resource_ownership_records (account_id, hid, time, state) VALUES (123, 'app123', '2011-12-01 00:12:00 UTC', 'inactive');
+CREATE OR REPLACE FUNCTION last_day(timestamptz)
+RETURNS timestamptz AS
+$$
+  SELECT (date_trunc('MONTH', $1) + INTERVAL '1 MONTH - 1 day');
+$$ LANGUAGE 'sql' IMMUTABLE STRICT;
 
-INSERT INTO billable_events (provider_id, rate_code_id, hid, event_id, time, state) VALUES (1, 1, 'app123', 1, '2010-12-01 00:12:00 UTC', 'open');
-INSERT INTO billable_events (provider_id, rate_code_id, hid, event_id, time, state) VALUES (1, 1, 'app124', 2, '2011-12-01 00:12:00 UTC', 'open');
+CREATE OR REPLACE FUNCTION first_day(timestamptz)
+RETURNS timestamptz AS
+$$
+  SELECT date_trunc('MONTH', $1);
+$$ LANGUAGE 'sql' IMMUTABLE STRICT;
+
+/*
+  BillableUnit
+  Setup correct from and to timestamps
+*/
 
 CREATE VIEW billable_units AS
   SELECT
@@ -29,11 +41,16 @@ CREATE VIEW billable_units AS
 ;
 
 CREATE VIEW resource_ownerships AS
-  SELECT a.account_id, a.hid, a.time as from, COALESCE(b.time, now()) as to
+  SELECT
+    a.account_id,
+    a.hid,
+    a.time as from,
+    COALESCE(b.time, now()) as to
+
     FROM resource_ownership_records a
     LEFT OUTER JOIN resource_ownership_records b
     ON
-          a.account_id  = b.account_id
+          a.event_id    = b.event_id
       AND a.state       = 'active'
       AND b.state       = 'inactive'
     WHERE
@@ -42,11 +59,60 @@ CREATE VIEW resource_ownerships AS
 
 CREATE VIEW billables AS
 SELECT
+  resource_ownerships.account_id,
   billable_units.hid,
-  GREATEST(billable_units.from, resource_ownerships.from) as from,
-  LEAST(billable_units.to, resource_ownerships.to) as to
+  GREATEST(billable_units.from, resource_ownerships.from, (SELECT * from first_day('2000-01-01'))) as from,
+  LEAST(billable_units.to, resource_ownerships.to, (SELECT * from last_day('2000-01-31'))) as to
   FROM billable_units
   INNER JOIN resource_ownerships
     ON
       billable_units.hid = resource_ownerships.hid
 ;
+
+/*
+  Test
+*/
+
+INSERT INTO accounts (id) VALUES (123);
+INSERT INTO accounts (id) VALUES (124);
+INSERT INTO providers (id) VALUES (1);
+INSERT INTO rate_codes (id) VALUES (1);
+
+
+INSERT INTO resource_ownership_records (event_id, account_id, hid, time, state) VALUES (1, 123, 'app123', '2000-01-01 00:00:00 UTC', 'active');
+INSERT INTO resource_ownership_records (event_id, account_id, hid, time, state) VALUES (1, 123, 'app123', '2000-01-01 00:12:00 UTC', 'inactive');
+INSERT INTO billable_events (provider_id, rate_code_id, hid, event_id, time, state) VALUES (1, 1, 'app123', 1, '2000-01-01 00:12:00 UTC', 'open');
+
+INSERT INTO resource_ownership_records (event_id, account_id, hid, time, state) VALUES (2, 123, 'app124', '1999-01-01 00:00:00 UTC', 'active');
+INSERT INTO billable_events (provider_id, rate_code_id, hid, event_id, time, state) VALUES (1, 1, 'app124', 2, '2000-01-01 00:00:00 UTC', 'open');
+INSERT INTO billable_events (provider_id, rate_code_id, hid, event_id, time, state) VALUES (1, 1, 'app124', 2, '2000-01-02 00:12:00 UTC', 'close');
+
+/*
+ SELECT * FROM billables;
+ account_id |  hid   |          from          |           to
+------------+--------+------------------------+------------------------
+        123 | app123 | 2000-01-01 00:12:00+00 | 2000-01-01 00:12:00+00
+        123 | app124 | 2000-01-01 00:00:00+00 | 2000-01-02 00:12:00+00
+*/
+
+-- Billable Event will span a month but ownership goes back and forth
+INSERT INTO billable_events (provider_id, rate_code_id, hid, event_id, time, state) VALUES (1, 1, 'app124', 3, '2000-01-01 00:00:00 UTC', 'open');
+
+INSERT INTO resource_ownership_records (event_id, account_id, hid, time, state) VALUES (3, 123, 'app124', '1999-01-01 00:00:00 UTC', 'active');
+INSERT INTO resource_ownership_records (event_id, account_id, hid, time, state) VALUES (3, 123, 'app124', '2000-01-02 00:00:00 UTC', 'inactive');
+
+INSERT INTO resource_ownership_records (event_id, account_id, hid, time, state) VALUES (4, 124, 'app124', '2000-01-02 00:00:00 UTC', 'active');
+INSERT INTO resource_ownership_records (event_id, account_id, hid, time, state) VALUES (4, 124, 'app124', '2000-01-10 00:00:00 UTC', 'inactive');
+
+INSERT INTO resource_ownership_records (event_id, account_id, hid, time, state) VALUES (5, 123, 'app124', '2000-01-10 00:00:00 UTC', 'active');
+
+/*
+ SELECT * FROM billables;
+
+ account_id |  hid   |          from          |           to
+------------+--------+------------------------+------------------------
+        123 | app124 | 2000-01-01 00:00:00+00 | 2000-01-02 00:00:00+00
+        124 | app124 | 2000-01-02 00:00:00+00 | 2000-01-10 00:00:00+00
+        123 | app124 | 2000-01-10 00:00:00+00 | 2000-01-31 00:00:00+00
+*/
+
