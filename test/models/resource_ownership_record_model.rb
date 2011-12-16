9e2275f35dc5402418aa451d468d9ee81dd57a46
Ryan R. Smith (ace hacker) <this.ryansmith@gmail.com>
2011-12-16 08:40:08 +0000

    use event_ids in the model test as well

--- test/models/resource_ownership_record_model.rb
+++ test/models/resource_ownership_record_model.rb
@@ -4,23 +4,23 @@ class ResourceOwnershipRecordTest < ShushuTest
 
   def test_collapse
     account = build_account
-    build_resource_ownership_record(:account_id => account.id, :hid => "123", :time => jan, :state => ResourceOwnershipRecord::Active)
+    build_resource_ownership_record(:event_id => "event1",:account_id => account.id, :hid => "123", :time => jan, :state => ResourceOwnershipRecord::Active)
     records = ResourceOwnershipRecord.collapse(account.id, jan, feb)
     assert(!records.empty?, "Expected to find some records")
   end
 
   def test_collapse_returns_from
     account = build_account
-    build_resource_ownership_record(:account_id => account.id, :hid => "123", :time => jan, :state => ResourceOwnershipRecord::Active)
-    records = ResourceOwnershipRecord.collapse(account.id, jan, feb).all
+    build_resource_ownership_record(:event_id => "event1",:account_id => account.id, :hid => "123", :time => jan, :state => ResourceOwnershipRecord::Active)
+    records = ResourceOwnershipRecord.collapse(account.id, jan, feb)
     record = records.first
     assert_equal(jan, record[:from])
   end
 
   def test_collapse_returns_from_and_to
     account = build_account
-    build_resource_ownership_record(:account_id => account.id, :hid => "123", :time => jan, :state => ResourceOwnershipRecord::Active)
-    build_resource_ownership_record(:account_id => account.id, :hid => "123", :time => feb, :state => ResourceOwnershipRecord::Inactive)
+    build_resource_ownership_record(:event_id => "event1", :account_id => account.id, :hid => "123", :time => jan, :state => ResourceOwnershipRecord::Active)
+    build_resource_ownership_record(:event_id => "event1", :account_id => account.id, :hid => "123", :time => feb, :state => ResourceOwnershipRecord::Inactive)
     records = ResourceOwnershipRecord.collapse(account.id, jan, feb)
     assert_equal(1, records.count)
     record = records.first
@@ -29,7 +29,7 @@ class ResourceOwnershipRecordTest < ShushuTest
 
   def test_collapse_returns_to_as_current_time_when_active
     account = build_account
-    build_resource_ownership_record(:account_id => account.id, :hid => "123", :time => jan, :state => ResourceOwnershipRecord::Active)
+    build_resource_ownership_record(:event_id => "event1",:account_id => account.id, :hid => "123", :time => jan, :state => ResourceOwnershipRecord::Active)
     records = ResourceOwnershipRecord.collapse(account.id, jan, feb)
     record = records.first
     assert_in_delta(Time.now, record[:to], 2)
