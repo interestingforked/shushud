bd03c2faeca0c288f6f841d73c35d3bc7aa9bbc4
Ryan R. Smith (ace hacker) <this.ryansmith@gmail.com>
2011-12-18 06:46:15 +0000

    stub total for now

--- lib/services/usage_report_service.rb
+++ lib/services/usage_report_service.rb
@@ -4,11 +4,13 @@ module UsageReportService
   def build_report(account_id, from, to)
     report = UsageReport.new(account_id, from, to)
     billable_units = report.billable_units.map(&:to_h)
+    #TODO Remove stubbed total
     {
       :account_id     => account_id,
       :from           => from,
       :to             => to,
-      :billable_units => billable_units
+      :billable_units => billable_units,
+      :total          => 99
     }
   end
 
