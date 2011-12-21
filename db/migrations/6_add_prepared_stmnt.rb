cf9fa0754a0e805c4a80258ff1c78d9baadb48c4
Ryan R. Smith (ace hacker) <this.ryansmith@gmail.com>
2011-12-21 02:29:51 +0000

    push our usage_report into the db as a function

---
+++ db/migrations/6_add_prepared_stmnt.rb
@@ -0,0 +1,51 @@
+Sequel.migration do
+  up do
+    execute(<<-EOD)
+
+      CREATE TYPE usage_report_type AS (
+        account_id int,
+        hid varchar(255),
+        from_timestamp timestamptz,
+        to_timestamp timestamptz,
+        qty numeric,
+        product_name varchar(255),
+        product_group varchar(255),
+        rate int,
+        rate_codes varchar(255)
+      );
+
+      CREATE OR REPLACE FUNCTION usage_report(int, timestamptz, timestamptz) RETURNS SETOF usage_report_type
+        AS $$
+          SELECT
+            resource_ownerships.account_id,
+            billable_units.hid,
+            GREATEST(billable_units.from, resource_ownerships.from, $2) as from_timestamp,
+            LEAST(billable_units.to, resource_ownerships.to, $3) as to_timestamp,
+            (
+              (extract('epoch' FROM
+                LEAST(billable_units.to, resource_ownerships.to, $3) - GREATEST(billable_units.from, resource_ownerships.from, $2)
+              )::numeric / (3600)) -- convert seconds into hours
+            ) as qty,
+            rate_codes.product_name,
+            rate_codes.product_group,
+            rate_codes.rate,
+            rate_codes.rate_period
+
+            FROM billable_units
+            LEFT OUTER JOIN rate_codes
+              ON
+                rate_codes.id = billable_units.rate_code_id
+            INNER JOIN resource_ownerships
+              ON
+                billable_units.hid = resource_ownerships.hid
+            WHERE
+              resource_ownerships.account_id = $1
+              AND
+                (billable_units.from, billable_units.to)
+                OVERLAPS
+                (resource_ownerships.from, resource_ownerships.to)
+        $$ LANGUAGE SQL
+      ;
+    EOD
+  end
+end
