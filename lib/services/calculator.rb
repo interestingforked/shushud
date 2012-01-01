6a417ad02046e4bc7ec4aca101153967d8b7a045
Ryan R. Smith (ace hacker) <this.ryansmith@gmail.com>
2012-01-01 23:57:07 +0000

    use reduce for computations

--- lib/services/calculator.rb
+++ lib/services/calculator.rb
@@ -3,8 +3,9 @@ module Calculator
 
   def total(units)
     units.group_by {|unit| unit["rate"].to_i}.map do |rate, units_by_rate|
-      rate * units_by_rate.inject(0) {|memo, unit| unit["qty"].to_f}
-    end.inject(0) {|memo, sub_total| sub_total}
+      sum_of_qty = units_by_rate.map {|u| u["qty"].to_f}.reduce(:+)
+      rate * sum_of_qty
+    end.reduce(:+)
   end
 
 end
