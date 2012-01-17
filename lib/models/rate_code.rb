30605be9de486d65c8bec8a1cfb0ce9fc2f18f4b
Ryan R. Smith (ace hacker) <this.ryansmith@gmail.com>
2012-01-17 02:52:09 +0000

    generate slug for rate code when none is given

--- lib/models/rate_code.rb
+++ lib/models/rate_code.rb
@@ -1,11 +1,16 @@
 class RateCode < Sequel::Model
 
+  def before_create
+    self.slug ||= SecureRandom.uuid
+    super
+  end
+
   def to_h
     {
-      :slug => self[:slug],
-      :rate => self[:rate],
+      :slug  => self[:slug],
+      :rate  => self[:rate],
       :group => self[:product_group],
-      :name => self[:product_name]
+      :name  => self[:product_name]
     }
   end
 
---
+++ test/models/rate_code_test.rb
@@ -0,0 +1,16 @@
+require File.expand_path('../../test_helper', __FILE__)
+
+class RateCodeTest < ShushuTest
+
+  def test_slug_generation
+    rate_code = RateCode.create
+    refute_nil rate_code.slug
+  end
+
+  def test_slug_generation_with_slug
+    rate_code = RateCode.create :slug => "OHHAI"
+    assert_equal "OHHAI", rate_code.slug
+  end
+
+
+end
