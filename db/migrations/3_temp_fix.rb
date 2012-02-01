6e5c8edd759fb0b66a7d4a709420775580553901
Ryan R. Smith (ace hacker) <this.ryansmith@gmail.com>
2012-02-01 12:42:28 -0800

    fix sequel syntax

--- db/migrations/3_temp_fix.rb
+++ db/migrations/3_temp_fix.rb
@@ -2,36 +2,36 @@ Sequel.migration do
   change do
 
     alter_table(:resource_ownership_records) do
-      foreign_key :provider_id, :providers
+      add_foreign_key :provider_id, :providers
     end
 
     alter_table(:account_ownership_records) do
-      foreign_key :provider_id, :providers
+      add_foreign_key :provider_id, :providers
     end
 
     alter_table(:receivables) do
-      foreign_key :provider_id, :providers
+      add_foreign_key :provider_id, :providers
     end
 
     alter_table(:card_tokens) do
-      foreign_key :provider_id, :providers
+      add_foreign_key :provider_id, :providers
     end
 
     alter_table(:payment_methods) do
-      foreign_key :provider_id, :providers
+      add_foreign_key :provider_id, :providers
     end
 
     alter_table(:payment_attempt_records) do
-      foreign_key :provider_id, :providers
+      add_foreign_key :provider_id, :providers
     end
 
     alter_table(:accounts) do
-      foreign_key :provider_id, :providers
+      add_foreign_key :provider_id, :providers
     end
 
     add_column :payment_methods, :slug, "varchar(255)"
     alter_table(:payment_methods) do
-      foreign_key :provider_id, :providers
+      add_foreign_key :provider_id, :providers
       add_unique_constraint([:provider_id, :slug])
     end
 
