14452736814e486f27aedff7ec5556cb52c019cc
Ryan R. Smith (ace hacker) <this.ryansmith@gmail.com>
2012-02-03 00:41:27 +0000

    fill in the open/close ordering test

--- test/acceptance/open_close_ordering_test.rb
+++ test/acceptance/open_close_ordering_test.rb
@@ -11,4 +11,43 @@ require File.expand_path('../../test_helper', __FILE__)
 # that a close is present when there is no open present.
 
 class OpenCloseOrderingTest < ShushuTest
+
+  def setup
+    super
+    @provider = build_provider(:token => "abc123")
+    authorize(@provider.id, "abc123")
+    @rate_code = build_rate_code(:provider_id => @provider.id)
+  end
+
+  def test_close_can_happen_before_open
+    put("resources/123/billable_events/1", {
+      :qty       => 1,
+      :rate_code => @rate_code.slug,
+      :state     => BillableEvent::Close,
+      :time      => "2012-01-01 00:00:01 UTC"
+    })
+    assert_equal(last_response.status, 201)
+  end
+
+  def test_usage_report_does_not_include_close_without_open
+    put("resources/123/billable_events/1", {
+      :qty       => 1,
+      :rate_code => @rate_code.slug,
+      :state     => BillableEvent::Close,
+      :time      => "2012-01-01 00:00:01 UTC"
+    })
+    assert_equal(last_response.status, 201)
+
+    account = build_account(:provider_id => @provider.id)
+    post("/accounts/#{account.id}/resource_ownerships/entity1", {
+      :hid => "123",
+      :time => "2012-01-01 00:00:00 UTC"
+    })
+    assert_equal 201, last_response.status
+
+    _, report = ReportService.usage_report(account.id, jan, feb)
+    billable_units = report[:billable_units]
+    assert_equal(0, billable_units.length)
+  end
+
 end
