3a424eabec9bad7ad247b18026ab5cc684c7a6a1
Ryan R. Smith (ace hacker) <this.ryansmith@gmail.com>
2012-02-23 21:23:28 +0000

    fix

--- lib/services/billable_event_service.rb
+++ lib/services/billable_event_service.rb
@@ -42,7 +42,7 @@ module BillableEventService
         :product_name     => args[:product_name],
         :description      => args[:description],
         :time             => args[:time],
-        :state            => BillableEvent.dec_state(state)
+        :state            => BillableEvent.enc_state(state)
       )
     end
   end
