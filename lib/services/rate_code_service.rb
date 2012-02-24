76a9952eefc3043ccdb9a2874b6f2f0c0ec0a121
Ryan R. Smith (ace hacker) <this.ryansmith@gmail.com>
2012-02-24 22:47:17 +0000

    normalize logging and exceptions

--- lib/services/rate_code_service.rb
+++ lib/services/rate_code_service.rb
@@ -3,10 +3,10 @@ module RateCodeService
 
   def find(slug)
     if rate_code = RateCode.find(:slug => slug)
-      Log.info("action=get_rate_code found rate_code=#{rate_code.id}")
+      Log.info(:action => "find_rate_code", :rate_code => rate_code.id)
       [200, rate_code.to_h]
     else
-      Log.info("action=get_rate_code not_found slug=#{slug}")
+      Log.error(:error => true, :action => "find_rate_code", :slug => slug)
       raise(Shushu::NotFound, "Could not find rate code with slug=#{slug}")
     end
   end
@@ -19,7 +19,10 @@ module RateCodeService
         target_id = args.delete(:target_provider_id)
         [201, create_record(args.merge(:provider_id => target_id))]
       else
-        raise(Shushu::AuthorizationError, "Provider is not authrozied to create rate_codes")
+        raise(
+          Shushu::AuthorizationError,
+          "Provider is not authrozied to create rate_codes"
+        )
       end
     end
   end
@@ -37,21 +40,25 @@ module RateCodeService
   def update(args)
     begin
       if rate_code = RateCode.find(:slug => args[:slug])
-        Log.info("action=update_rate_code rate_code=#{rate_code.id}")
+        Log.info(:action => "update_rate_code", :rate_code => "#{rate_code.id}")
         rate_code.set(
           :rate          => args[:rate],
           :product_group => args[:product_group],
           :product_name  => args[:product_name]
         )
         if rate_code.save(:raise_on_failure => false)
-          Log.info("action=update_rate_code rate_code=#{rate_code.id} saved")
+          Log.info(:action=>"update_rate_code",:rate_code=>"#{rate_code.id}")
           [200, rate_code]
         else
-          raise(RuntimeError, "rate_code=#{rate_code.id} args=#{args[:rate_code]} Failed to save changes")
+          raise(RuntimeError,"rate code save fail args=#{args[:rate_code]}")
         end
       else
-        Log.info("action=update_rate_code not_found rate_code_id=#{args[:rate_code_id]}")
-        raise(Shushu::NotFound, "Could not find rate_code with slug=#{args[:slug]}")
+        Log.error({
+          :error => true,
+          :action => "update_rate_code",
+          :message => "rate code not found",
+        }.merge(args))
+        raise(Shushu::NotFound, "rate_code not found slug=#{args[:slug]}")
       end
     rescue Sequel::Error => e
       raise(RuntimeError, e.message)
