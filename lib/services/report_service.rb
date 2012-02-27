45d08cf4edc0d7d3fe91174fd4ffd155cae541aa
Ryan R. Smith (ace hacker) <this.ryansmith@gmail.com>
2012-02-27 17:55:27 +0000

    normalize logging

--- lib/services/report_service.rb
+++ lib/services/report_service.rb
@@ -2,34 +2,49 @@ module ReportService
   extend self
 
   def usage_report(account_id, from, to)
-    Log.info("#usage_report_requested account=#{account_id} from=#{from} to=#{to}")
-    billable_units = exec_sql("SELECT * FROM usage_report($1, $2, $3)", account_id, from, to)
-    res = {
-      :account_id     => account_id,
-      :from           => from,
-      :to             => to,
-      :billable_units => billable_units,
-      :total          => Calculator.total(billable_units)
-    }
-    Log.info("#usage_report_success account=#{account_id} from=#{from} to=#{to}")
-    [200, res]
+    Log.info_t(
+      :action     => "usage_report",
+      :account_id => account_id,
+      :from       => from,
+      :to         => to
+    ) do
+      s = "SELECT * FROM usage_report($1, $2, $3)"
+      billable_units = exec_sql(s, account_id, from, to)
+      [200, {
+        :account_id     => account_id,
+        :from           => from,
+        :to             => to,
+        :billable_units => billable_units,
+        :total          => Calculator.total(billable_units)
+      }]
+    end
   end
 
   def invoice(payment_method_id, from, to)
-    billable_units = exec_sql("SELECT * FROM invoice($1, $2, $3)", payment_method_id, from, to)
-    res = {
-      :payment_method_id => payment_method_id,
-      :from              => from,
-      :to                => to,
-      :billable_units    => billable_units,
-      :total             => Calculator.total(billable_units)
-    }
-    [200, res]
+    Log.info_t(
+        :action         => "usage_report",
+        :payment_method => payment_method_id,
+        :from           => from,
+        :to             => to
+      ) do
+      s = "SELECT * FROM invoice($1, $2, $3)"
+      billable_units = exec_sql(s, payment_method_id, from, to)
+      [200, {
+        :payment_method_id => payment_method_id,
+        :from              => from,
+        :to                => to,
+        :billable_units    => billable_units,
+        :total             => Calculator.total(billable_units)
+      }]
+    end
   end
 
   def rev_report(from, to)
-    total = exec_sql("SELECT * from rev_report($1, $2)", from, to).first["rev_report"]
-    [200, {:time => Time.now, :total => total}]
+    Log.info_t(:action => "rev_report", :from => from, :to => to) do
+      s = "SELECT * from rev_report($1, $2)"
+      total = exec_sql(s, from, to).first["rev_report"]
+      [200, {:time => Time.now, :total => total}]
+    end
   end
 
   def exec_sql(sql, *args)
@@ -37,4 +52,5 @@ module ReportService
       conn.exec(sql, args).to_a
     end
   end
+
 end
