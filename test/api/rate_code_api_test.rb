3178cc68c31a3070a15a25cb14d19018a4487e75
Ryan R. Smith (ace hacker) <this.ryansmith@gmail.com>
2012-02-28 21:58:47 +0000

    normalize syntax in test file

--- test/api/rate_code_api_test.rb
+++ test/api/rate_code_api_test.rb
@@ -5,40 +5,30 @@ class RateCodeApiTest < ShushuTest
   def setup
     super
     @provider = build_provider(:token => "abc123")
-  end
-
-  def setup_auth
-    authorize @provider.id, "abc123"
+    authorize(@provider.id, "abc123")
   end
 
   def test_create_rate_code
-    setup_auth
-
-    post "/rate_codes"
-    assert_equal 201, last_response.status
+    post("/rate_codes")
+    assert_equal(201, last_response.status)
   end
 
   def test_find_rate_code_can_succeed
-    setup_auth
-
     RateCode.create(:provider_id => @provider.id, :slug => "rt01", :rate => 5)
-    get "/rate_codes/rt01"
-    assert_equal 200, last_response.status
+    get("/rate_codes/rt01")
+    assert_equal(200, last_response.status)
   end
 
   def test_find_rate_code_can_404
-    setup_auth
-
-    get "/rate_codes/not_A_rate_code"
-    assert_equal 404, last_response.status
+    get("/rate_codes/not_A_rate_code")
+    assert_equal(404, last_response.status)
   end
 
   def test_update_rate_code
-    setup_auth
     RateCode.create(:provider_id => @provider.id, :slug => "rt01", :rate => 5)
 
-    put "/rate_codes/rt01", {:rate_code => {:rate => 10}}
-    assert_equal 200, last_response.status
+    put("/rate_codes/rt01", {:rate_code => {:rate => 10}})
+    assert_equal(200, last_response.status)
   end
 
 end
