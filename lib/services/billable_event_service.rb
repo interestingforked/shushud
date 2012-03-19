e421f2e2ab4e2456799b7f4c74be6c5495abf8e3
Ryan Smith (ace hacker) <ryandotsmith@32k.io>
2012-03-19 18:44:51 +0000

    log params when we can not write the record

--- lib/services/billable_event_service.rb
+++ lib/services/billable_event_service.rb
@@ -28,18 +28,23 @@ module BillableEventService
   end
 
   def create_record(state, args)
-    Log.info_t({:action => "#{state}_event"}.merge(args)) do
-      BillableEvent.create(
-        :provider_id      => args[:provider_id],
-        :rate_code_id     => args[:rate_code_id],
-        :entity_id        => args[:entity_id],
-        :hid              => args[:hid],
-        :qty              => args[:qty],
-        :product_name     => args[:product_name],
-        :description      => args[:description],
-        :time             => args[:time],
-        :state            => BillableEvent.enc_state(state)
-      )
+    Log.info_t({:info => true, :action => "#{state}_event"}.merge(args)) do
+      begin
+        BillableEvent.create(
+          :provider_id      => args[:provider_id],
+          :rate_code_id     => args[:rate_code_id],
+          :entity_id        => args[:entity_id],
+          :hid              => args[:hid],
+          :qty              => args[:qty],
+          :product_name     => args[:product_name],
+          :description      => args[:description],
+          :time             => args[:time],
+          :state            => BillableEvent.enc_state(state)
+        )
+      rescue StandardError => e
+        Log.error({:error => true, :action => "#{state}_event"}.merge(args))
+        raise(e)
+      end
     end
   end
 
