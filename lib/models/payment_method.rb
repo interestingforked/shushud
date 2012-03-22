f618511ba4d0eb82b14b53db636074326835b9a7
Ryan Smith (ace hacker) <ryandotsmith@32k.io>
2012-03-22 03:49:54 +0000

    describe race condition

--- lib/models/payment_method.rb
+++ lib/models/payment_method.rb
@@ -1,6 +1,8 @@
 class PaymentMethod < Sequel::Model
 
   def card_token
+    #TODO should consider race condition when 2 Shushus
+    # write a card_token at the exact same time.
     CardToken.
     filter(:payment_method_id => self[:id]).
     order(:created_at).
