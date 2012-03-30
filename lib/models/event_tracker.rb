0c58132aec24c276145e526b8adcf169dc6aed12
Ryan Smith (ace hacker) <ryandotsmith@32k.io>
2012-03-30 03:30:26 +0000

    be explicit about delete

--- lib/models/event_tracker.rb
+++ lib/models/event_tracker.rb
@@ -4,7 +4,7 @@ module EventTracker
   def track(entity_id, state)
     if entity_id
       if r = Shushu::DB[:open_events].first(:entity_id => entity_id)
-        r.destroy
+        Shushu::DB[:open_events].filter(:entity_id => r[:entity_id]).delete
       else
         Shushu::DB[:open_events].insert(:entity_id => entity_id, :state => state)
       end
