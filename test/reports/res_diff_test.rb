928fdd205a1b912ffa6ca10d6db3ddfb9cf0f7fc
Chris Continanza <christopher.continanza@gmail.com>
2012-04-05 21:29:42 +0000

    move stuff out of methods for clarity

--- test/reports/res_diff_test.rb
+++ test/reports/res_diff_test.rb
@@ -12,11 +12,13 @@ class ResDiffReportTest < ShushuTest
   end
 
   def test_res_diff_with_increased
-    t0, t1, t2, t3 = make_events
+    t0, t1, t2, t3 = build_events
     params = {lfrom: t0, lto: t1, rfrom: t1, rto: t2, delta_increasing: 1, lrev_zero: 0, rrev_zero: 0}
 
     get('/res_diff/resources', params) 
     assert_equal(200, last_response.status)
+
+    report = JSON.parse(last_response.body)
     assert_equal(1, report['resources'].length)
 
     resource = report['resources'].pop
@@ -29,11 +31,13 @@ class ResDiffReportTest < ShushuTest
   end
 
   def test_res_diff_with_new
-    t0, t1, t2, t3 = make_events
+    t0, t1, t2, t3 = build_events
     params = {lfrom: t0, lto: t1, rfrom: t1, rto: t2, delta_increasing: 1, lrev_zero: 1, rrev_zero: 0}
 
     get('/res_diff/resources', params) 
     assert_equal(200, last_response.status)
+
+    report = JSON.parse(last_response.body)
     assert_equal(1, report['resources'].length)
 
     resource = report['resources'].pop
@@ -46,11 +50,13 @@ class ResDiffReportTest < ShushuTest
   end
 
   def test_res_diff_with_attrition
-    t0, t1, t2, t3 = make_events
+    t0, t1, t2, t3 = build_events
     params = {lfrom: t0, lto: t1, rfrom: t1, rto: t2, delta_increasing: 0, lrev_zero: 0, rrev_zero: 1}
 
     get('/res_diff/resources', params) 
     assert_equal(200, last_response.status)
+
+    report = JSON.parse(last_response.body)
     assert_equal(1, report['resources'].length)
 
     resource = report['resources'].pop
@@ -63,11 +69,13 @@ class ResDiffReportTest < ShushuTest
   end
 
   def test_res_diff_with_decreased
-    t0, t1, t2, t3 = make_events
+    t0, t1, t2, t3 = build_events
     params = {lfrom: t1, lto: t2, rfrom: t2, rto: t3, delta_increasing: 0, lrev_zero: 0, rrev_zero: 0}
 
     get('/res_diff/resources', params)
     assert_equal(200, last_response.status)
+
+    report = JSON.parse(last_response.body)
     assert_equal(1, report['resources'].length)
 
     resource = report['resources'].pop
@@ -85,9 +93,12 @@ class ResDiffReportTest < ShushuTest
 
     get('/res_diff/resources', params.merge(limit: 1)) 
     assert_equal(200, last_response.status)
+
+    report = JSON.parse(last_response.body)
     assert_equal(1, report['resources'].length)
 
     get('/res_diff/resources', params)
+    report = JSON.parse(last_response.body)
     assert_equal(200, last_response.status)
     assert_equal(2, report['resources'].length)
   end
@@ -100,28 +111,18 @@ class ResDiffReportTest < ShushuTest
   end
 
   private
-  def time
-    t0 = Time.mktime(2012,1)
-    t1 = t0 + 60 * 60 * 24
-    t2 = t1 + 60 * 60 * 24
-    t3 = t2 + 60 * 60 * 24
-    [t0, t1, t2, t3]
-  end
-
-  def uuids
-    4.times.map { SecureRandom.uuid }
-  end
-
-  def report
-    JSON.parse(last_response.body)
+  def assert_agg_diff(n)
+    assert_equal(200, last_response.status)
+    agg = JSON.parse(last_response.body)
+    assert_equal(n, agg['diff'].to_f)
   end
 
   # app123 increases t1 -> t2 then decreases t2 -> t3
   # app124 is new in t1 -> t2
   # app125 is attrition in t1 -> t2
-  def make_events
+  def build_events
     t0, t1, t2, t3 = time
-    eid1, eid2, eid3, eid4 = uuids
+    eid1, eid2, eid3, eid4 = 4.times.map { SecureRandom.uuid }
     rate_code = build_rate_code(:rate => 5)
     build_billable_event("app123", eid1, 1, t0, rate_code.slug)
     build_billable_event("app123", eid2, 1, t1, rate_code.slug)
@@ -136,7 +137,7 @@ class ResDiffReportTest < ShushuTest
   # app123 and app124 both are new in t1 -> t2
   def build_multiple_new
     t0, t1, t2, t3 = time
-    eid1, eid2     = uuids
+    eid1, eid2     = 2.times.map { SecureRandom.uuid }
     rate_code = build_rate_code(:rate => 5)
     build_billable_event("app124", eid1, 1, t1, rate_code.slug)
     build_billable_event("app124", eid1, 0, t2, rate_code.slug)
@@ -145,9 +146,11 @@ class ResDiffReportTest < ShushuTest
     [t0, t1, t2, t3].map{ |t| t.strftime('%Y-%m-%d') }
   end
 
-  def assert_agg_diff(n)
-    assert_equal(200, last_response.status)
-    agg = JSON.parse(last_response.body)
-    assert_equal(n, agg['diff'].to_f)
+  def time
+    t0 = Time.mktime(2012,1)
+    t1 = t0 + 60 * 60 * 24
+    t2 = t1 + 60 * 60 * 24
+    t3 = t2 + 60 * 60 * 24
+    [t0, t1, t2, t3]
   end
 end
