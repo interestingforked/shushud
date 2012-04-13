c1c0f366105c30302b737f55c35fa8f8d2116e01
Ryan Smith (ace hacker) <ryandotsmith@32k.io>
2012-04-12 22:43:44 -0700

    make transformer safe for concurrency

--- db/ddl/transformer.sql
+++ db/ddl/transformer.sql
@@ -1,56 +1,66 @@
 CREATE OR REPLACE FUNCTION
-transform()
+transform(
+  max_transformers int,
+  transformer_id int
+)
 RETURNS void
-AS $$
-  INSERT INTO close_events(
-    entity_id,
-    provider_id,
-    resource_id,
-    "from",
-    "to",
-    qty,
-    rate,
-    rate_period,
-    product_group,
-    product_name,
-    created_at
-  )
-  SELECT
-    a.entity_id_uuid,
-    a.provider_id,
-    a.hid,
-    a.time,
-    b.time,
-    (extract('epoch' from (b.time - a.time))),
-    rate_codes.rate,
-    rate_codes.rate_period,
-    rate_codes.product_group,
-    COALESCE(a.product_name, rate_codes.product_name),
-    now()
-  FROM
-    billable_events a
-  INNER JOIN
-    billable_events b
-    ON a.entity_id = b.entity_id
-    AND a.state = 1 AND b.state = 0
-  INNER JOIN
-    rate_codes
-    ON rate_codes.id = a.rate_code_id
-  WHERE
-    a.recorded_at IS NULL
-    AND
-    b.recorded_at IS NULL
-  FOR UPDATE OF a, b NOWAIT
-  LIMIT 100;
-
-  UPDATE
-    billable_events
-  SET
-    recorded_at = now()
-  FROM
-    close_events
-  WHERE
-    close_events.entity_id = billable_events.entity_id_uuid
-    AND
-    billable_events.recorded_at IS NULL;
-$$ LANGUAGE SQL ;
+AS $function$
+  DECLARE
+    event RECORD;
+  BEGIN
+    FOR event IN EXECUTE $insert$
+      INSERT INTO close_events(
+        entity_id,
+        provider_id,
+        resource_id,
+        "from",
+        "to",
+        qty,
+        rate,
+        rate_period,
+        product_group,
+        product_name,
+        created_at
+      )
+      SELECT
+        a.entity_id_uuid,
+        a.provider_id,
+        a.hid,
+        a.time,
+        b.time,
+        (extract('epoch' from (b.time - a.time))),
+        rate_codes.rate,
+        rate_codes.rate_period,
+        rate_codes.product_group,
+        COALESCE(a.product_name, rate_codes.product_name),
+        now()
+      FROM
+        billable_events a
+      INNER JOIN
+        billable_events b
+        ON a.entity_id = b.entity_id
+        AND a.state = 1 AND b.state = 0
+      INNER JOIN
+        rate_codes
+        ON rate_codes.id = a.rate_code_id
+      WHERE
+        a.recorded_at IS NULL
+        AND
+        b.recorded_at IS NULL
+        AND
+      $insert$ || ' MOD(a.id,' ||  max_transformers || ') = ' || transformer_id
+               || ' FOR UPDATE OF a, b NOWAIT '
+               || ' LIMIT 100 '
+               || ' RETURNING entity_id'
+    LOOP
+      UPDATE
+        billable_events
+      SET
+        recorded_at = now()
+      WHERE
+        entity_id_uuid = event.entity_id
+        AND
+        billable_events.recorded_at IS NULL;
+    END LOOP;
+  END;
+$function$ LANGUAGE plpgsql;
