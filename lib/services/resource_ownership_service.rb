05227dd3a188a0c8c065c887311171aa0e07a784
Ryan Smith (ace hacker) <ryan@heroku.com>
2012-05-10 05:36:39 +0000

    make it fit on 80

--- lib/services/resource_ownership_service.rb
+++ lib/services/resource_ownership_service.rb
@@ -11,11 +11,32 @@ module ResourceOwnershipService
   end
 
   def activate(provider_id, account_id, resource_id, time, entity_id)
-    [200, create_record(provider_id, account_id, resource_id, ResourceOwnershipRecord::ACTIVE, time, entity_id)]
+    [
+      200,
+      create_record(
+        provider_id,
+        account_id,
+        resource_id,
+        ResourceOwnershipRecord::ACTIVE,
+        time,
+        entity_id
+      )
+    ]
   end
 
   def deactivate(provider_id, account_id, resource_id, time, entity_id)
-    [200, create_record(provider_id, account_id, resource_id, ResourceOwnershipRecord::INACTIVE, time, entity_id)]
+    [
+      200,
+      create_record(
+        provider_id,
+        account_id,
+        resource_id,
+        ResourceOwnershipRecord::INACTIVE,
+        time,
+        entity_id
+      )
+    ]
+
   end
 
   def create_record(provider_id, account_id, resource_id, state, time, entity_id)
