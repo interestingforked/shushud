9631d7f92bdbda544e565a23debecf9aa8425bc0
♠ ace hacker <ryan@heroku.com>
2012-08-07 15:54:32 -0700

    use shushu module

--- lib/utils.rb
+++ lib/utils.rb
@@ -26,8 +26,8 @@ module Utils
     if @reporter.nil?
       email = Shushu::Config.librato_email
       token = Shushu::Config.librato_token
-      opts = {prefix: APP_NAME,
-        interval: 30, source: "#{APP_NAME}.herokuapp.com"}
+      opts = {prefix: Shushu::APP_NAME,
+        interval: 30, source: "#{Shushu::APP_NAME}.herokuapp.com"}
       if email && token
         @reporter = Metriks::Reporter::LibratoMetrics.new(email, token, opts)
         @reporter.start
